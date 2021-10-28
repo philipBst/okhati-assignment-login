dd868f8bb890169bcbdbefc815f7dd23e9c82a63
philipBst <philipbasnet@gmail.com>
2021-10-28 12:24:47 +0545

    Simple login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,44 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { TextField } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import { FormControlLabel } from "@material-ui/core";
+import { Checkbox } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Happy coding!!!
-        </p>
-      </header>
+    <div className="form-control">
+      <div className="header">
+        <img
+          src="https://img.jobsnepal.com/big/WaCUSC916hn7vJ5dP23aretB7Y5uNMc16IMhav1N.jpeg"
+          alt="okhati"
+          className="logo"
+        />
+        <h2>Okhati</h2>
+      </div>
+      <form>
+        <div className="field">
+          <TextField
+            id="username"
+            label="Username"
+            variant="outlined"
+          ></TextField>
+        </div>
+        <div className="field">
+          <TextField
+            id="password"
+            label="Password"
+            variant="outlined"
+          ></TextField>
+        </div>
+        <div className="field">
+          <FormControlLabel control={<Checkbox />} label="Keep me logged in" />
+        </div>
+        <div className="field">
+          <Button className="btn" variant="contained">
+            Login
+          </Button>
+        </div>
+      </form>
     </div>
   );
 }
